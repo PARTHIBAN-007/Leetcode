02e48fbaa00c53e2eef666c54dca21a89f6d71ca
PARTHIBAN-007 <parthiban.ktech@gmail.com>
2026-01-01 20:20:47 +0530

    Linked List + Hash + Prefix Sum

---
+++ Leetcode Quest/3_Association_Slope/Linked_List/Q1 Remove Duplicates from Sorted List.py
@@ -0,0 +1,43 @@
+"""
+Remove Duplicates from Sorted List
+
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+"""
+
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ptr = head
+        while ptr:
+            if ptr.next and ptr.next.val==ptr.val:
+                ptr.next = ptr.next.next
+            else:
+                ptr = ptr.next
+        return head
